4cefe85eec6a02169e8a1c849d61821951a8d3a3
niedzwiedzw <niedzwiedzwo@gmail.com>
2020-12-18 14:49:01 +0100

    pokazuje composition api

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="App">
+    {{ helloMessage }}
+      <button @click="updateName">Od tylu</button>
+      <input placeholder="imie boże" type="text" v-model="name" />
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script lang="ts">
+import { defineComponent, ref, Ref, computed } from "vue";
+
+function useMessageLogic(text: Ref<string>) {
+    const helloMessage = computed(() => {
+        return `this is ${text.value}'s app do chuja`;
+    });
+    const updateName = () => {
+        text.value = text.value.split('').reverse().join('');
+    };
+    return {
+        helloMessage,
+        updateName,
+    }
 }
 
-#nav {
-  padding: 30px;
+export default defineComponent({
+  name: "App",
+  props: {},
+  setup() {
+      const name = ref("Paweł");
+      // function helloMessage() {
+      //     return `this is ${name.value}'s app do chuja`;
+      // }
+      // function updateName() {
+      //     name.value = name.value.split('').reverse().join('');
+      // }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+      return {
+          name,
+          ...useMessageLogic(name),
+      };
+  },
+});
+</script>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style scoped lang="scss">
+.App {
 }
 </style>
